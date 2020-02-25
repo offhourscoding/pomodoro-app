ad8169f7ce6a8d77890e155a9dd7672649ac671c
Matt Adams <matt@offhourscoding.com>
2020-02-25 02:06:32 -0500

    Created list and actions for each todo

--- frontend/src/app/modules/todo-list/components/todo-list/todo-list.component.ts
+++ frontend/src/app/modules/todo-list/components/todo-list/todo-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../interfaces';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-todo-list',
@@ -7,9 +9,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
+  todoList: Todo[];
+
   constructor() { }
 
+
   ngOnInit() {
+    this.todoList = [
+      {
+        id: 1,
+        task: 'Task 1',
+        order: 4
+      },
+      {
+        id: 2,
+        task: 'Task 2',
+        order: 3
+      },
+      {
+        id: 3,
+        task: 'Task 3',
+        order: 2
+      },
+      {
+        id: 4,
+        task: 'Task 4',
+        order: 1
+      }
+    ];
+
+    this.reorderTodoList();
   }
 
+  //// Button Handlers ////
+  
+  deleteClick(id: number) {
+    let index = this.findTodoIndex(id);
+    if (index > -1) {
+      this.todoList.splice(index, 1);
+      this.reorderTodoList();
+      console.log(this.todoList);
+    }
+  }
+
+
+  //// Event Listeners ////
+
+  changeCompleteEvent(id: number) {
+    let index = this.findTodoIndex(id);
+    if (index !== -1) {
+      this.todoList[index].completed = !this.todoList[index].completed;
+    }
+  }
+
+
+  //// Helpers ////
+  
+  findTodoIndex(id: number): number {
+    return this.todoList.findIndex(todo => todo.id === id);
+  }
+
+  reorderTodoList() {
+    this.todoList = _.orderBy(this.todoList, 'order');
+
+    for(let i = 0; i < this.todoList.length; i++) {
+      this.todoList[i].order = i + 1;
+    }
+  }
 }
