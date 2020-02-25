a4af2eeafcfeeaa91dd5211e8fe33f3b68a57d0c
Matt Adams <matt@offhourscoding.com>
2020-02-25 02:04:45 -0500

    Added icon and checkbox material modules

--- frontend/src/app/material.module.ts
+++ frontend/src/app/material.module.ts
@@ -5,10 +5,12 @@ import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
-  imports: [ MatListModule, MatExpansionModule, MatInputModule, MatCardModule, MatButtonModule, MatCardModule, ],
-  exports: [ MatListModule, MatExpansionModule, MatInputModule, MatCardModule, MatButtonModule, MatCardModule, ]
+  imports: [ MatCheckboxModule, MatIconModule, MatListModule, MatExpansionModule, MatInputModule, MatCardModule, MatButtonModule, MatCardModule, ],
+  exports: [ MatCheckboxModule, MatIconModule, MatListModule, MatExpansionModule, MatInputModule, MatCardModule, MatButtonModule, MatCardModule, ]
 })
 
 export class MaterialModule { }
